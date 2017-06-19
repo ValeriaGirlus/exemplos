ce5e2240fa8f942e519c4019406b617b5526808d
ValeriaGirlus <valeriagirlus@gmail.com>
2017-06-19 19:15:44 +0100

    numeros de x a y

--- src/io/altar/exemplos/test/NumerosdeXaY.java
+++ src/io/altar/exemplos/test/NumerosdeXaY.java
@@ -2,4 +2,27 @@ package io.altar.exemplos.test;
 
 public class NumerosdeXaY {
 
+	/*
+	 Desenvolva um programa que imprima os números de x a y, no entanto deverá 
+	 imprimir Fizz sempre que os números sejam múltiplos de 3, 
+	 Buzz quando múltiplos de 5 e FizzBuzz quando múltiplos de 3 e 5.
+	 */
+	public static void main(String [] args){
+		int x = 0;
+		int y = 15;
+		
+		for(int i=x; i <= y; i++){
+			System.out.println(i);
+		
+			if(i % 3 == 0){
+				System.out.println("Fizz");
+			}
+			if(i % 5 == 0){
+				System.out.println("Buzz");
+			}
+			if(i % 3 == 0 && i % 5 == 0 ){
+				System.out.println("FizzBuzz");
+			}
+		}
+	}
 }
