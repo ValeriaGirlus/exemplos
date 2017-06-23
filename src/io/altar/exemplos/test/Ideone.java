15b9e451434b5704cd1c502f52a025c7ec55a428
ValeriaGirlus <valeriagirlus@gmail.com>
2017-06-23 12:55:39 +0100

    sdasa

---
+++ src/io/altar/exemplos/test/Ideone.java
@@ -0,0 +1,15 @@
+package io.altar.exemplos.test;
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Ideone {
+	
+	
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	// your code goes here
+	}
+	}	
