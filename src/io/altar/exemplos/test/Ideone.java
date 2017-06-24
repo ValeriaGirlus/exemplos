949d2afe0fe8d9561d5a3c3202104ce2e50b6a5f
ValeriaGirlus <valeriagirlus@gmail.com>
2017-06-24 23:04:01 +0100

    sgsy

--- src/io/altar/exemplos/test/Ideone.java
+++ src/io/altar/exemplos/test/Ideone.java
@@ -1,6 +1,5 @@
 package io.altar.exemplos.test;
 
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
@@ -9,16 +8,29 @@ public class Ideone {
 		
 	public static void main (String[] args) throws java.lang.Exception 
 	{
-		Scanner s = new Scanner(System.in);
-		String input = null;
-		String a = "jjjooohhh";
-		System.out.println("suggest name");
-		input = s.nextLine();
+//		Scanner s = new Scanner(System.in);
+//		String a  = "aaabbbccc";
+//		String input = null;
+//		
+//		System.out.println("suggest name");
+//		input = s.nextLine();
 		
-		if(input == a){
-			System.out.println("Like");
-		}else {
-			System.out.println("Unlike");
-		}	
+		String input  = "aaabbbcc";
+		LinkedHashMap<String, Integer> characternumber = new LinkedHashMap<>();
+		
+		for(int i = 0, n = input.length(); i < n; i++ ){
+			
+			if(characternumber.containsKey(input.charAt(i))){
+				Integer value = 
+				
+			}
+		}
+		
+		
+//		if(input.matches(a)){
+//			System.out.println("Like");
+//		}else {
+//			System.out.println("Unlike");
+//		}	
 	}
 }	
